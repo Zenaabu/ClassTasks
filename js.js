5278182370960260959b9139bf61480e3abfda90
Zenaabu <zenaabu11@gmail.com>
2025-03-26 16:55:17 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+// second questions
+let name = prompt('Enter your name', 'text');
+console.log(name);
+let isBoss = confirm('Are you sure you want to delete the product?');
+if (isBoss) {
+  console.log('The product was deleted');
+}
+
+// first questions
+document.writeln('Zena');
+document.writeln('AboMfleh');
+
+console.log('Zena');
+console.log('AboMfleh');
+
+const myArr = ['Ronaldo', 'Robert', 'Dan'];
+console.log(myArr);
